f77e8fb5f0b1ff6eedc5eba8d08891350835cb9a
richelle.soares <richelle.soares@targetsistemas.com.br>
2024-05-06 23:15:22 -0300

    Conectando o Avatar do usuário do Supabass ao StreamChat

--- src/app/(home)/(tabs)/index.tsx
+++ src/app/(home)/(tabs)/index.tsx
@@ -1,10 +1,12 @@
 import { ChannelList } from "stream-chat-expo";
 import { router } from "expo-router";
+import { useAuth } from "../../../providers/AuthProvider";
 
 export default function MainTabScreen() {
-    
+    const { user } = useAuth(); 
     return ( 
         <ChannelList 
+        filters={{ members: { $in: [user.id] }}}
             onSelect={(channel) => router.push(`/channel/${channel.cid}`)} 
         />  
     );
